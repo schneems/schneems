4c70a5d77db132f914432790515cad3cff2b061b
schneems <richard.schneeman@gmail.com>
2015-04-10 12:01:51 -0500

    Set RSS to serve UTF8 information

---
+++ _plugins/utf8.rb
@@ -0,0 +1,18 @@
+module Jekyll
+  module Commands
+    class Serve
+
+      class << self
+        alias :_original_webrick_options :webrick_options
+      end
+
+      def self.webrick_options(config)
+        options = _original_webrick_options(config)
+        options[:MimeTypes].merge!({'html' => 'text/html; charset=utf-8'})
+        options[:MimeTypes].merge!({'xml'  => 'text/xml; charset=utf-8'})
+        options
+      end
+
+    end
+  end
+end
