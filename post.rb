db544a842fdc35ef35480f001b5c6f0039880778
schneems <richard.schneeman@gmail.com>
2016-11-21 22:02:52 -0600

    New post generator

---
+++ post.rb
@@ -0,0 +1,65 @@
+#!/usr/bin/env ruby
+
+# Generate a new jekyll blog post using prompts then place it in the _posts folder.
+
+require 'active_support/inflector'
+
+puts 'Enter the post title:'
+title = gets.chomp
+title.titleize
+
+year = Time.now.strftime('%Y')
+month = Time.now.strftime('%m')
+day = Time.now.strftime('%d')
+
+hyphen_date = "#{year.to_s}-#{month.to_s}-#{day.to_s}"
+time = Time.now.strftime('%H:%M')
+
+puts 'Enter the category:'
+category = gets.chomp
+
+puts 'Is this a link post? (y/N)'
+is_link = gets.chomp
+
+post_has_link = false
+
+if is_link == 'y'
+  post_has_link = true
+end
+
+sanitized_title = title.downcase.gsub(/[^a-z0-9\s]/i, '')
+
+dirty_slug = sanitized_title.split(' ')
+clean_slug = []
+
+dirty_slug.each do |s|
+  clean_slug << s
+  clean_slug << '-'
+end
+
+clean_slug.pop
+
+final_slug = clean_slug.join('')
+
+the_post_permalink = "/#{year}/#{month}/#{day}/#{final_slug}/"
+
+# Create the _posts directory
+
+system('mkdir', '_posts') unless File.exists?('_posts')
+
+the_post_file_name = hyphen_date + '-' + final_slug.to_s + '.md'
+the_post_file = File.new("_posts/#{the_post_file_name}", 'w')
+the_post_file.puts('---')
+the_post_file.puts("title: \"#{title}\"")
+the_post_file.puts('layout: post')
+the_post_file.puts('published: true')
+the_post_file.puts("date: #{hyphen_date}")
+the_post_file.puts("permalink: #{the_post_permalink}")
+the_post_file.puts('categories:')
+the_post_file.puts("    - #{category}")
+the_post_file.puts('link: ') if post_has_link
+the_post_file.puts('---')
+the_post_file.puts('')
+the_post_file.close
+
+system('atom', "_posts/#{the_post_file_name}")
