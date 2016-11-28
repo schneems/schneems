e6811ccecd728ab124b1e02c0dabd4366513f2d7
schneems <richard.schneeman@gmail.com>
2016-11-28 16:53:22 -0600

    Mobile friendly youtube embed

---
+++ assets/javascripts/youtube-wrap.js
@@ -0,0 +1,19 @@
+// Mobile Friendly Youtube Embed via JavaScript: https://codepen.io/schneems/pen/woreoR
+//
+window.onload = function() {
+  var elementList = document.querySelectorAll("iframe");
+  elementList.forEach(function(elem) {
+
+    // Only apply to Youtube players
+    var src = elem.getAttribute('src');
+    if (src && src.indexOf('youtube') !== -1) {
+      var div = document.createElement('div');
+      div.innerHTML = elem.outerHTML;
+      div.setAttribute('class', 'video-container');
+
+      elem.parentNode.insertBefore(div, elem);
+      elem.remove();
+    }
+
+  });
+}
